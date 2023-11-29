080e17bb9424e411926f377f5ce73b0e2b0a1ace
Benedikt Beigang <benedikt.beigang@stud.htwk-leipzig.de>
2023-11-29 17:58:04 +0100

    #142 feat: reconnecting implemented

--- Services/SubleticClientService.cs
+++ Services/SubleticClientService.cs
@@ -11,7 +11,6 @@ public class SubleticClientService : BackgroundService
     private const int MAX_RECEIVABLE_CHARACTER_LENGTH_OF_SUBTITLES_IN_KILOBYTE = 4;
     private readonly string exportFilePath;
     private readonly IConfiguration configuration;
-    private ClientWebSocket client;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="SubleticClientService"/> class.
@@ -20,15 +19,25 @@ public class SubleticClientService : BackgroundService
     public SubleticClientService(IConfiguration configuration)
     {
         this.configuration = configuration;
-        client = new ClientWebSocket();
         exportFilePath = "ReceivedSubtitles." + configuration.GetValue<string>("SubleticClientSettings:SubtitleFormat");
     }
 
     /// <inheritdoc/>
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        await Init_WebSocket(stoppingToken);
-        Task receiveTask = ReceiveMessages(stoppingToken);
+        while (stoppingToken.IsCancellationRequested is false)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} - Trying to connect to Subletic...");
+            await ConnectToSubletic(stoppingToken);
+            await Task.Delay(1000); // Wait for one second before attempting reconnect.
+        }
+    }
+
+    private async Task ConnectToSubletic(CancellationToken stoppingToken)
+    {
+        ClientWebSocket client = new ClientWebSocket();
+        await Init_WebSocket(stoppingToken, client);
+        Task receiveTask = ReceiveMessages(stoppingToken, client);
 
         try
         {
@@ -49,36 +58,37 @@ public class SubleticClientService : BackgroundService
 
             await receiveTask;
         }
-        catch (NullReferenceException)
-        {
-            Console.WriteLine("Please provide a valid video name in appsettings.json");
-        }
         catch (Exception e)
         {
-            Console.WriteLine(e.Message);
+            switch (e)
+            {
+                case NullReferenceException: Console.WriteLine("Please provide a valid video name in appsettings.json"); break;
+                case ObjectDisposedException: break;
+                default: Console.WriteLine(e.Message); break;
+            }
         }
     }
 
-    private async Task Init_WebSocket(CancellationToken stoppingToken)
+    private async Task Init_WebSocket(CancellationToken stoppingToken, ClientWebSocket client)
     {
         try
         {
             string targetWebSocketUrl = Environment.GetEnvironmentVariable("BACKEND_WEBSOCKET_URL") ?? "ws://localhost:40114/transcribe";
             await client.ConnectAsync(new Uri(targetWebSocketUrl), stoppingToken);
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} - Connected to Subletic.");
         }
-        catch (NullReferenceException)
+        catch (Exception e)
         {
-            Console.WriteLine("Please provide a valid WebSocket URL in appsettings.json");
-            return;
-        }
-        catch (WebSocketException)
-        {
-            Console.WriteLine("No connection to the WebSocket server possible. Please check the URL in appsettings.json");
-            return;
+            switch (e)
+            {
+                case NullReferenceException: Console.WriteLine("Please provide a valid WebSocket URL in appsettings.json"); break;
+                case WebSocketException: Console.WriteLine("No connection to the WebSocket server possible. Please check the URL in appsettings.json"); break;
+                case ObjectDisposedException: Console.WriteLine("Backend not available."); break;
+            }
         }
     }
 
-    private async Task ReceiveMessages(CancellationToken stoppingToken)
+    private async Task ReceiveMessages(CancellationToken stoppingToken, ClientWebSocket client)
     {
         var buffer = new byte[1024 * MAX_RECEIVABLE_CHARACTER_LENGTH_OF_SUBTITLES_IN_KILOBYTE];
 
